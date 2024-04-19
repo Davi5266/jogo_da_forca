1279457056e77eb96ccd396498dfc67e277d5bf0
Davi Feliz <117379990+Davi5266@users.noreply.github.com>
2024-04-19 13:11:24 -0300

    Update main.py
    descrições das funções

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from random import *
 
 lista = ['banana','carro','paz','agua','bomba','chocolate','morango']
+# A função 'randrange' está sendo importada da bibliote random que esta escolhendo aleatóriamente as 7 palvras presentes na lista
 ponteiro = lista[randrange(7)]
+
+# A função 'len' é para visualizar a quatidade de elementos presentes na string
+# O video tem uma explicação mais simples e curta para entendimento
+# https://www.youtube.com/watch?v=QxjArQ9xZDg&pp=ygUfcHl0aG9uIGNvbW8gc29sZXRyYXIgdW1hIHN0cmluZw%3D%3D
 
 print(len(ponteiro))
 print(ponteiro)
